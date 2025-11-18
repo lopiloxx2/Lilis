a19506b96de3ef9e4c4b0ea3d616b6b01365839c
lopiloxx2 <lovito.diego@gmail.com>
2025-11-18 19:16:59 -0300

    Delete lilis directory
    se eliminara carpeta malula

--- lilis/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.views import LogoutView
-
-urlpatterns = [
-    path('', views.login_registro_view, name='login_registro'),
-   # path('logout/', LogoutView.as_view(next_page='login_registro'), name='logout'),
-]
--- lilis/proveedores/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_proveedores, name='lista_proveedores'),
-    path('nuevo/', views.crear_proveedor, name='crear_proveedor'),
-    path('editar/<int:pk>/', views.editar_proveedor, name='editar_proveedor'),
-    path('eliminar/<int:pk>/', views.eliminar_proveedor, name='eliminar_proveedor'),
-]
